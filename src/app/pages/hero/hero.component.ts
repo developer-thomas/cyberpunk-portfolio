151076d392d9511a4c6bb0e310447ae051f84928
Thomas Edson <andepipous@gmail.com>
2025-04-18 18:27:35 -0400

    feat: create hero component

---
+++ src/app/pages/hero/hero.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+
+@Component({
+  selector: 'app-hero-page',
+  imports: [
+    HeroSectionComponent
+  ],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss'
+})
+export class HeroPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  scrollToDashboard(): void {
+    const dashboardElement = document.getElementById("dashboard")
+    if (dashboardElement) {
+      dashboardElement.scrollIntoView({ behavior: "smooth" })
+    }
+  }
+}
