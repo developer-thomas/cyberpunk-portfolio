c38c4c3c7a1dfbf6bbd6cd64d734956e31adcb2d
Thomas Edson <andepipous@gmail.com>
2025-09-20 08:33:08 -0400

    add env ex

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    // production: true,
+    // emailJsServiceId: 'service_eq2khhz',
+    // emailJsTemplateId: 'template_qmhpynp',
+    // emailJsAutoReplyTemplateId: 'template_9xmkj07',
+    // emailJsPublicKey: 'gmxYSvVDj8H7pB-9_'
+    production: true,
+    emailJsServiceId: '',
+    emailJsTemplateId: '',
+    emailJsAutoReplyTemplateId: '',
+    emailJsPublicKey: ''
+  };
+  
