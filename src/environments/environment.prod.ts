02fee4818b15c56150acdd9741657d3b3518baf6
Thomas Edson <andepipous@gmail.com>
2025-09-20 09:22:03 -0400

    fix: adjust environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
-    // production: true,
-    // emailJsServiceId: 'service_eq2khhz',
-    // emailJsTemplateId: 'template_qmhpynp',
-    // emailJsAutoReplyTemplateId: 'template_9xmkj07',
-    // emailJsPublicKey: 'gmxYSvVDj8H7pB-9_'
-    production: true,
-    emailJsServiceId: process.env['NG_APP_EMAILJS_SERVICE_ID'],
-    emailJsTemplateId: process.env['NG_APP_EMAILJS_TEMPLATE_ID'],
-    emailJsAutoReplyTemplateId: process.env['NG_APP_EMAILJS_AUTOREPLY_TEMPLATE_ID'],
-    emailJsPublicKey: process.env['NG_APP_EMAILJS_PUBLIC_KEY']
-  };
+  production: true,
+  emailJsPublicKey: 'gmxYSvVDj8H7pB-9_',
+  apiBaseUrl: '/.netlify/functions'
+};
   
