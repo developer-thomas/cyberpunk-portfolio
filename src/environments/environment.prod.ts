4b9089d53d57d82b36d61ce2a0dda597cf87d3ee
Thomas Edson <andepipous@gmail.com>
2025-09-20 08:47:21 -0400

    fix environments variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,9 +5,9 @@ export const environment = {
     // emailJsAutoReplyTemplateId: 'template_9xmkj07',
     // emailJsPublicKey: 'gmxYSvVDj8H7pB-9_'
     production: true,
-    emailJsServiceId: '',
-    emailJsTemplateId: '',
-    emailJsAutoReplyTemplateId: '',
-    emailJsPublicKey: ''
+    emailJsServiceId: process.env['NG_APP_EMAILJS_SERVICE_ID'],
+    emailJsTemplateId: process.env['NG_APP_EMAILJS_TEMPLATE_ID'],
+    emailJsAutoReplyTemplateId: process.env['NG_APP_EMAILJS_AUTOREPLY_TEMPLATE_ID'],
+    emailJsPublicKey: process.env['NG_APP_EMAILJS_PUBLIC_KEY']
   };
   
